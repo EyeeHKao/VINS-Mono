f862359be62c8166ec1e20208be50425582945a2
EyeeH <990457372@qq.com>
2019-03-11 20:57:46 +0800

    临时注释

--- feature_tracker/src/feature_tracker.h
+++ feature_tracker/src/feature_tracker.h
@@ -46,18 +46,18 @@ class FeatureTracker
 
     void undistortedPoints();
 
-    cv::Mat mask;
-    cv::Mat fisheye_mask;
-    cv::Mat prev_img, cur_img, forw_img;
-    vector<cv::Point2f> n_pts;
-    vector<cv::Point2f> prev_pts, cur_pts, forw_pts;
-    vector<cv::Point2f> prev_un_pts, cur_un_pts;
-    vector<cv::Point2f> pts_velocity;
-    vector<int> ids;
+    cv::Mat mask;   ///图像mask,用于取感兴趣部分的图像
+    cv::Mat fisheye_mask; ///鱼眼形的mask
+    cv::Mat prev_img, cur_img, forw_img; ///前一帧， 当前帧， 下一帧图像
+    vector<cv::Point2f> n_pts;  ///特征点集
+    vector<cv::Point2f> prev_pts, cur_pts, forw_pts;  ///前一帧， 当前帧， 下一帧特征点集
+    vector<cv::Point2f> prev_un_pts, cur_un_pts; ///前一帧， 当前帧 未追踪/匹配的特帧点集
+    vector<cv::Point2f> pts_velocity; 点速度
+    vector<int> ids; ///特征点匹配关系
     vector<int> track_cnt;
     map<int, cv::Point2f> cur_un_pts_map;
     map<int, cv::Point2f> prev_un_pts_map;
-    camodocal::CameraPtr m_camera;
+    camodocal::CameraPtr m_camera;  ///相机模型
     double cur_time;
     double prev_time;
 
